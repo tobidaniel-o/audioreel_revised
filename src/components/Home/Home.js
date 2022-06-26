e9c08c64ad8fae02e8bbbe9f14ca8123b901e136
Tobi Okunuga <tobi.okunuga@bmo.com>
2022-06-26 11:33:44 -0400

    created the navbar and added a bg image to the homepage header

---
+++ src/components/Home/Home.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react'
+import Navigation from '../Navigation/Navigation'
+import './Home.scss'
+
+
+
+const Home = () => {
+  return (
+    <Fragment>
+        <header>
+            <Navigation />
+        </header>
+    </Fragment>
+  )
+}
+
+export default Home
